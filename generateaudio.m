243399d9e6d3412592736dc714a36236d6bfc213
MarceloTM-2001 <machotm@hotmail.com>
2024-09-30 23:25:33 -0600

    Modificacion audios para pruebas

--- generateaudio.m
+++ generateaudio.m
@@ -1,5 +1,4 @@
 pkg load signal
-% Parámetros
 fs = 48000;              % Frecuencia de muestreo
 t = 0:1/fs:15;           % Tiempo de 0 a 15 segundos
 f_start = 20;           % Frecuencia inicial
@@ -9,8 +8,14 @@ f_end = 20000;          % Frecuencia final
 signal = chirp(t, f_start, t(end), f_end);
 audiowrite('AudioFiles/barrido_frecuencias.wav', signal, fs);
 
+f_end = 5000;          % Frecuencia final
 t = 0:1/fs:10;
-% Generar ruido blanco
+
+% Barrido de frecuencias
+signal = chirp(t, f_start, t(end), f_end);
+audiowrite('AudioFiles/barrido_frecuencias_5k.wav', signal, fs);
+
+
 noise = randn(size(t));
 
 % Normalizar el ruido para que esté entre -1 y 1
