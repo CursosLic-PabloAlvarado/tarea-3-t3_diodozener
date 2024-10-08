8af3d04052e52c5c7b044d19799ceb7abf6e6e92
DARD172002 <rayodev16@gmail.comn>
2024-10-08 17:30:40 -0600

    filtros.pdf

---
+++ graficas.m
@@ -0,0 +1,30 @@
+
+Fs=48000;
+
+#viewfreqresp("Mats/ellip_lowpass.mat", Fs)
+#viewfreqresp("Mats/cheby2_lowpass.mat", Fs)
+#viewfreqresp("Mats/cheby1_lowpass.mat", Fs)
+#viewfreqresp("Mats/butter_lowpass.mat", Fs)
+
+
+
+# High Pass
+#viewfreqresp("Mats/ellip_highpass.mat", Fs)
+#viewfreqresp("Mats/cheby2_highpass.mat", Fs)
+#viewfreqresp("Mats/cheby1_highpass.mat", Fs)
+#viewfreqresp("Mats/butter_highpass.mat", Fs)
+
+
+
+
+#bandpass
+#viewfreqresp("Mats/ellip_bandpass.mat", Fs)
+#viewfreqresp("Mats/cheby1_bandpass.mat", Fs)
+#viewfreqresp("Mats/cheby2_bandpass.mat", Fs)
+#viewfreqresp("Mats/butter_bandpass.mat", Fs)
+
+
+#viewfreqresp("Mats/ellip_stopband.mat", Fs)
+#viewfreqresp("Mats/cheby1_stopband.mat", Fs)
+#viewfreqresp("Mats/cheby2_stopband.mat", Fs)
+viewfreqresp("Mats/butter_stopband.mat", Fs)
